c918a435b51b69ac9b06de5be6837f03734a98f0
fenugrec <fenugrec@users.sourceforge.net>
2020-11-30 11:45:16 -0500

    add ROM signature generation (failing)
    doesnt match expected sigs

--- sigtest.c
+++ sigtest.c
@@ -1,8 +1,11 @@
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "crc.h"
+#include "stypes.h"
 
 const char symbols[]="0123456789ACFHPU";
 
@@ -131,12 +134,86 @@ void test_algo(void) {
 
 }
 
+
+// hax, get file length but restore position
+u32 flen(FILE *hf) {
+	long siz;
+	long orig;
+
+	if (!hf) return 0;
+	orig = ftell(hf);
+	if (orig < 0) return 0;
+
+	if (fseek(hf, 0, SEEK_END)) return 0;
+
+	siz = ftell(hf);
+	if (siz < 0) siz=0;
+		//the rest of the code just won't work if siz = UINT32_MAX
+	#if (LONG_MAX >= UINT32_MAX)
+		if ((long long) siz == (long long) UINT32_MAX) siz = 0;
+	#endif
+
+	if (fseek(hf, orig, SEEK_SET)) return 0;
+	return (u32) siz;
+}
+
+
+#define FILE_MAXSIZE 64*1024UL
+void rom_printsigs(FILE *i_file) {
+	u32 file_len;
+
+	rewind(i_file);
+	file_len = flen(i_file);
+	if ((!file_len) || (file_len > FILE_MAXSIZE)) {
+		printf("huge file (length %lu)\n", (unsigned long) file_len);
+		return;
+	}
+
+	u8 *src = malloc(file_len);
+	if (!src) {
+		printf("malloc choke\n");
+		return;
+	}
+
+	/* load whole ROM */
+	if (fread(src,1,file_len,i_file) != file_len) {
+		printf("trouble reading\n");
+		free(src);
+		return;
+	}
+
+	printf("clocks: 0x%05X\n", file_len);
+	unsigned databit;
+	for (databit = 0; databit <= 7; databit++) {
+		//for every bit position : generate signature as if probing signal
+		unsigned cnt;
+		u16 crc = 0;
+		for (cnt = 0; cnt < file_len; cnt++) {
+			unsigned newbit = (src[cnt] >> databit) & 1;
+			crc = crc1281_singlebit(newbit, crc);
+		}
+		printf("D%u : ", databit);
+		print_sig(crc);
+		printf("\n");
+	}
+	return;
+}
+
 int main(int argc, char **argv) {
-	(void) argc;
-	(void) argv;
-	
-	test_algo();	
+	FILE *romfile;
 
+	if (argc != 2) {
+		printf("usage : %s <romfile>\n", argv[0]);
+		test_algo();
+		return 0;
+	}
 
+	romfile = fopen(argv[1], "rb");
+	if (!romfile) {
+		printf("fopen() failed: %s\n", strerror(errno));
+		return -1;
+	}
+	rom_printsigs(romfile);
+	fclose(romfile);
 	return 0;
 }
